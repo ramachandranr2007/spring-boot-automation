766a8183271a6c34883efbef2dd173d85030f693
Ramachandran R <ramachandranr2007@gmail.com>
2024-01-16 00:25:16 +0100

    Update

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,5 +1,9 @@
 package org.example;
 
+import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.SpringBootConfiguration;
@@ -9,12 +13,16 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 @SpringBootConfiguration
+@Epic("Login Tests Epic")
+@Feature("Invalid Login Features")
 public class AppTest
 {
     String actualText = "Hello JUnit 5";
 
     @DisplayName("Test Spring @Autowired Integration")
     @Test
+    @Story("User tries to login the system with invalid username and invalid password.")
+    @Description("Invalid Login Test with Invalid Username and Invalid Password.")
     void testGet() {
         assertEquals("Hello JUnit 5", actualText);
     }
